da9fb841cfee163c43dea4a246712cce33840fc3
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 11:11:53 -0300

    Quinto Exercicio

--- src/components/exercicio5/index.jsx
+++ src/components/exercicio5/index.jsx
@@ -1,5 +1,18 @@
+import "./ex5.css";
+
 function Exercicio5() {
-	return <h1 className='title'>Exercicio5</h1>
+  const animalArray = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
+
+  return (
+    <>
+      <h1 className="title">Exercicio5</h1>
+      <ul className="list">
+        {animalArray.map((animal) => (
+          <li className="list__item">{animal}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default Exercicio5
+export default Exercicio5;
