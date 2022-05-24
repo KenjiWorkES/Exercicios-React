fa121a616e572c9573d413531dc72add109d69b3
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 11:54:08 -0300

    Sétimo Exercicio

--- src/components/exercicio7/index.jsx
+++ src/components/exercicio7/index.jsx
@@ -1,5 +1,50 @@
+import React, { useState } from "react";
+
+import "./ex7.css";
+
 function Exercicio7() {
-	return <h1 className='title'>Exercicio7</h1>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    alert("Hello " + firstName + " " + lastName);
+    setFirstName("");
+    setLastName("");
+  };
+
+  const firstNameChangeHandler = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setLastName(event.target.value);
+  };
+
+  return (
+    <>
+      <h1 className="title">Exercicio7</h1>
+      <form className="greet" onSubmit={submitHandler}>
+        <input
+          onChange={firstNameChangeHandler}
+          className="greet__input"
+          value={firstName}
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          onChange={lastNameChangeHandler}
+          className="greet__input"
+          value={lastName}
+          type="text"
+          placeholder="Last Name"
+        />
+        <button className="greet__button" type="submit">
+          GREET ME
+        </button>
+      </form>
+    </>
+  );
 }
 
-export default Exercicio7
+export default Exercicio7;
