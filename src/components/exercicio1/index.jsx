eb672da13b3b374f0f16c0ee987a5700db0c3016
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 10:32:09 -0300

    Primeiro Exercicio

--- src/components/exercicio1/index.jsx
+++ src/components/exercicio1/index.jsx
@@ -1,5 +1,14 @@
+import "./ex1.css";
+
 function Exercicio1() {
-	return <h1 className='title'>Exercicio1</h1>
+  return (
+    <>
+      <h1 className="title">Exercicio1</h1>
+      <div className="square">
+        <p className="square__text">Hello, World!</p>
+      </div>
+    </>
+  );
 }
 
-export default Exercicio1
+export default Exercicio1;
