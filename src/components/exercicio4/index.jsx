8b244bd5b46b135f1b0c3f342ae56e8885f360eb
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 11:06:37 -0300

    Quarto Exercicio

--- src/components/exercicio4/index.jsx
+++ src/components/exercicio4/index.jsx
@@ -1,5 +1,26 @@
+import "./ex4.css";
+import React, { useState } from "react";
+
 function Exercicio4() {
-	return <h1 className='title'>Exercicio4</h1>
+  const [cont, setCont] = useState(0);
+
+  const incrementHandler = () => {
+    setCont(cont + 1);
+  };
+
+  return (
+    <>
+      <h1 className="title">Exercicio4</h1>
+      <div className="cont-button">
+        <p className="cont-button__text">
+          Button has been clicked: {cont} times
+        </p>
+        <button onClick={incrementHandler} className="cont-button__btn">
+          Click me!
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Exercicio4
+export default Exercicio4;
