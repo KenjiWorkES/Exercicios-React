b62c1e7e23d0128e1a1a2099d25a7dfcaf20642b
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 13:29:55 -0300

    Oitavo Exercicio

--- src/components/exercicio8/index.jsx
+++ src/components/exercicio8/index.jsx
@@ -1,5 +1,32 @@
+import "./ex8.css";
+
 function Exercicio8() {
-	return <h1 className='title'>Exercicio8</h1>
+  const jokesData = [
+    {
+      id: 1,
+      setup: "What's the best thing about a Boolean?",
+      punchline: "Even if you're wrong, you're only off by a bit",
+    },
+    {
+      id: 2,
+      setup: "Why do programmers wear glasses?",
+      punchline: "Because they need to C#",
+    },
+  ];
+
+  return (
+    <>
+      <h1 className="title">Exercicio8</h1>
+      <div className="jokes">
+        {jokesData.map((joke) => (
+          <div id={joke.id} key={joke.id} className="jokes__item">
+            <h2 className="jokes__question">{joke.setup}</h2>
+            <p className="jokes__answer">{joke.punchline}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Exercicio8
+export default Exercicio8;
