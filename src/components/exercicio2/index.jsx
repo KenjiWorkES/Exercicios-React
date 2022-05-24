6513764e50c2d70b9a0fc01846616f1737b7f65a
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-24 10:53:13 -0300

    Segundo Exercicio

--- src/components/exercicio2/index.jsx
+++ src/components/exercicio2/index.jsx
@@ -1,5 +1,20 @@
+import "./ex2.css";
+
+const clickHandler = () => {
+  alert("Clicked!");
+};
+
 function Exercicio2() {
-	return <h1 className='title'>Exercicio2</h1>
+  return (
+    <>
+      <h1 className="title">Exercicio2</h1>
+      <div className="alert-button">
+        <button onClick={clickHandler} className="alert-button__btn">
+          Click Me!
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Exercicio2
+export default Exercicio2;
